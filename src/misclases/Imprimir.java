9f8e236f8b144f54b2e05d21b5989aafbbc30868
Cristobal <kris_todosinfin@outlook.com>
2021-04-24 11:26:43 -0500

    Inicio del programa

---
+++ src/misclases/Imprimir.java
@@ -0,0 +1,12 @@
+
+
+package misclases;
+
+public class Imprimir {
+    
+    
+    public void imprime(){
+        System.out.println("Hola mundo...");
+    }
+    
+}
---
+++ src/mispruebas/Test.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mispruebas;
+
+import misclases.Imprimir;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Test {
+    
+    public static void main(String[] args) {
+        Imprimir obj=new Imprimir();
+        obj.imprime();
+        
+        
+    }
+}
